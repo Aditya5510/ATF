b539725ef21e4d549f89c560299de171ee55f5be
Aditya <swaroopaditya545@gmail.com>
2024-07-22 12:29:09 +0530

    dip

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ env.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://navigwall.vercel.app"],
+    origin: ["http://localhost:5173", "https://atffrontend.vercel.app"],
   })
 );
 
